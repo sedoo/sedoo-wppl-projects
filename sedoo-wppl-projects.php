f21f839466772652a4e80150db99acf1b189a5b4
ngruwe <nicolas.gruwe@obs-mip.fr>
2021-04-07 14:21:19 +0200

    tag and release

--- sedoo-wppl-projects.php
+++ sedoo-wppl-projects.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Sedoo - Projects
  * Description: Créer un post type Projects Sedoo
- * Version: 1.0.2
+ * Version: 1.0.4
  * Author: Nicolas Gruwe 
  * GitHub Plugin URI: sedoo/sedoo-wppl-projects
  * GitHub Branch:     master
